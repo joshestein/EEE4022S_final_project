d5c09ee6207c8d950339c125a0800e29f9a4526f
Josh <josh.e.stein@gmail.com>
2019-07-30 17:12:31 +0200

    Laser projection onto pixels.

--- laser_projection.m
+++ laser_projection.m
@@ -1,16 +1,16 @@
 SDK_DIR = '../../Datasets/robotcar-dataset-sdk-2.1.1';
 DATA = '../../Datasets/robotcar-dataset';
+path(path, [SDK_DIR '/matlab/'])
 
 IMAGE_DIR = [DATA '/stereo/centre/'];
 LIDAR_DIR = [DATA '/ldmrs/'];
 INS_FILE = [DATA '/gps/ins.csv'];
 EXTRINSIC_DIR = [SDK_DIR '/extrinsics/'];
+MODELS_DIR = [SDK_DIR '/models/'];
 
-test_img = '1418381798076682';
-path(path, SDK_DIR);
-path(path, DATA);
+timestamp = 1418381799701502;
 
-img = LoadImage(IMAGE_DIR, test_img);
-%imshow(img)
+% img = LoadImage(IMAGE_DIR, timestamp);
+% BuildPointcloud(LIDAR_DIR, INS_FILE, EXTRINSIC_DIR);
 
-BuildPointcloud(LIDAR_DIR, INS_FILE, EXTRINSIC_DIR);
+ProjectLaserIntoCamera(IMAGE_DIR, LIDAR_DIR, INS_FILE, MODELS_DIR, EXTRINSIC_DIR, timestamp);
