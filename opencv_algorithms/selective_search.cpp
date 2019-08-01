77fa0aa43c3255ec729c890898a29671af519c42
Josh <josh.e.stein@gmail.com>
2019-08-01 17:53:54 +0200

    Moved drawing rects into independent function.

--- opencv_algorithms/selective_search.cpp
+++ opencv_algorithms/selective_search.cpp
@@ -16,6 +16,8 @@ void help() {
     "./selective_search input_image (s|[f]|q)" << std::endl;
 }
 
+void draw_rects(Mat &img, const int &num_rects, std::vector<Rect> &rects);
+
 int main(int argc, char** argv) {
     if (argc < 2) {
         help();
@@ -31,6 +33,7 @@ int main(int argc, char** argv) {
     int height = 500;
     int width = img.cols*height/img.rows;
     resize(img, img, Size(width, height));
+
 
     Ptr<SelectiveSearchSegmentation> ss = createSelectiveSearchSegmentation();
     ss->setBaseImage(img);
@@ -58,22 +61,17 @@ int main(int argc, char** argv) {
     std::vector<Rect> rects;
     ss->process(rects);
     int num_rects = 10;
+
     std::cout << "Num regions: " << rects.size() << std::endl;
     std::cout << "Enter 'm'/'l' for more/less rects, 'q' to quit.\n" ;
 
     char c = (char)waitKey();
     while (c != 'q') {
-        // required for updating rects
+        // keep original img for proper updating rects
         Mat clone_img = img.clone();
-        int i = 0;
 
-        for (auto it = rects.begin(); it != rects.end(); ++it) {
-            if (i++ < num_rects) {
-                rectangle(clone_img, *it, Scalar(0, 0, 255));
-            }
-        }
+        draw_rects(clone_img, num_rects, rects);
 
-        imshow("Output", clone_img);
         c = (char)waitKey();
         if (c == 'm') {
             num_rects += 10;
@@ -84,4 +82,14 @@ int main(int argc, char** argv) {
         
     }
     return 0;
+}
+
+void draw_rects(Mat &img, const int &num_rects, std::vector<Rect> &rects) {
+    int rects_drawn = 0;
+    for (auto it = rects.begin(); it != rects.end(); ++it) {
+        if (rects_drawn++ < num_rects) {
+            rectangle(img, *it, Scalar(0, 0, 255));
+        }
+    }
+    imshow("Selective Search", img);
 }
