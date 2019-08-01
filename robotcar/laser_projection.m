be8fff59397e4d1ec32e0d03b2dfe7fbac608edb
Josh <josh.e.stein@gmail.com>
2019-08-01 17:54:50 +0200

    Switched to real (non-sample) data.

--- robotcar/laser_projection.m
+++ robotcar/laser_projection.m
@@ -1,5 +1,6 @@
 SDK_DIR = '../../Datasets/robotcar-dataset-sdk-2.1.1';
-DATA = '../../Datasets/robotcar-dataset';
+% DATA = '../../Datasets/robotcar-dataset';
+DATA = '../../Datasets/2014-05-14-13-53-47';
 path(path, [SDK_DIR '/matlab/'])
 
 IMAGE_DIR = [DATA '/stereo/centre/'];
@@ -8,8 +9,10 @@ INS_FILE = [DATA '/gps/ins.csv'];
 EXTRINSIC_DIR = [SDK_DIR '/extrinsics/'];
 MODELS_DIR = [SDK_DIR '/models/'];
 
-start_timestamp = 1418381799701502;
-end_timestamp = 1418381799826501;
+sample_start_timestamp = 1418381799701502;
+sample_end_timestamp = 1418381799826501;
+
+start_timestamp = 1400075815389497;
 
 % img = LoadImage(IMAGE_DIR, start_timestamp);
 % imshow(img);
