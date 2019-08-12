5f1d951559513ee07ab37ad2d7d26bcd3efb0753
Josh <josh.e.stein@gmail.com>
2019-08-12 18:07:59 +0200

    Realization that dense map is from lms_front, not LD-MRS

--- robotcar/laser_projection.m
+++ robotcar/laser_projection.m
@@ -1,23 +1,32 @@
 SDK_DIR = '../../Datasets/robotcar-dataset-sdk-2.1.1';
-% DATA = '../../Datasets/robotcar-dataset';
-DATA = '../../Datasets/2014-05-14-13-53-47';
+% sample_DATA = '../../Datasets/robotcar-dataset';
+% sample_start_timestamp = 1418381799701502;
+% sample_end_timestamp = 1418381799826501;
+
+% 2014_DATA = '../../Datasets/2014-05-14-13-53-47';
+% 2014_start_timestamp = 1400075815389497;
+% 5 frames later...
+% 2014_end_timestamp = 1400075815701972;
+
+DATA = '/home/josh/Documents/UCT/Thesis/Datasets/2015-11-10-10-32-52';
+%start_timestamp = 1447151694756261;
+end_timestamp = start_timestamp+10e7;
+% start_timestamp = 1447151634694061;
+start_timestamp =  1447151671318873;
+% next_ frame: 1447151671443870
+% next_next_frame: 1447151671506369
+% 5 frames later: 1447151671631368
+% another: 1447151685006313
 path(path, [SDK_DIR '/matlab/'])
 
 IMAGE_DIR = [DATA '/stereo/centre/'];
-LIDAR_DIR = [DATA '/ldmrs/'];
+LIDAR_DIR = [DATA '/lms_front/'];
 INS_FILE = [DATA '/gps/ins.csv'];
 EXTRINSIC_DIR = [SDK_DIR '/extrinsics/'];
 MODELS_DIR = [SDK_DIR '/models/'];
 
-% sample_start_timestamp = 1418381799701502;
-% sample_end_timestamp = 1418381799826501;
-
-start_timestamp = 1400075815389497;
-% 5 frames later...
-% start_timestamp = 1400075815701972;
-
 % img = LoadImage(IMAGE_DIR, start_timestamp);
 % imshow(img);
-% BuildPointcloud(LIDAR_DIR, INS_FILE, EXTRINSIC_DIR, sample_start_timestamp, sample_end_timestamp);
+% BuildPointcloud(LIDAR_DIR, INS_FILE, EXTRINSIC_DIR, start_timestamp);
 
 ProjectLaserIntoCamera(IMAGE_DIR, LIDAR_DIR, INS_FILE, MODELS_DIR, EXTRINSIC_DIR, start_timestamp);
