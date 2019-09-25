9794a5543df33faf41e5759702761d6e54b6e5ac
Josh <josh.e.stein@gmail.com>
2019-09-25 21:30:37 +0200

    Correct person mask. Comments.

--- evaluation/ground_truth.m
+++ evaluation/ground_truth.m
@@ -6,7 +6,7 @@ segmentation_dir = "/home/josh/Documents/UCT/Thesis/Code/kitti/full_run/drive_27
 no_merge_dir = "/home/josh/Documents/UCT/Thesis/Code/kitti/full_run/drive_27/no_merge/";
 interactive_dir = "/home/josh/Documents/UCT/Thesis/Code/kitti/full_run/drive_27/no_merge/interactive/";
 
-% frame = 172;
+frame = 166;
 files = dir(gt_dir);
 num_files = size(files);
 
@@ -27,29 +27,32 @@ for i = 1:num_files
     gt_img = imread(sprintf('%s%06d.png', gt_dir, frame));
     img = imread(sprintf('%s%06d.png', base_dir, frame));
 
+    % read segmentation masks from disk
     frame_1_mask = load(sprintf('%s%d_mask.mat', frame_integration_1, frame));
     frame_2_mask = load(sprintf('%s%d_mask.mat', frame_integration_2, frame));
     no_integration_mask = load(sprintf('%s%d_mask.mat', segmentation_dir, frame));
     no_merge_mask = load(sprintf('%s%d_mask.mat', no_merge_dir, frame));
     interactive_mask = load(sprintf('%s%d_interactive_mask.mat', interactive_dir, frame));
 
+    % convert masks to logical arrays
     frame_1_mask = logical(frame_1_mask.mask);
     frame_2_mask = logical(frame_2_mask.mask);
     no_integration_mask = logical(no_integration_mask.mask);
     no_merge_mask = logical(no_merge_mask.mask);
     interactive_mask = logical(interactive_mask.mask);
 
-    % figure;
-
     r = gt_img(:,:,1);
     g = gt_img(:,:,2);
     b = gt_img(:,:,3);
 
+    % find import things in ground truth masks
     car_mask = ((r == 64) & (g == 0) & (b == 128));
-    person_mask = ((r == 63) & (g == 63) & (b == 0));
+    person_mask = ((r == 64) & (g == 64) & (b == 0));
 
+    % combine things of importance
     full_mask = car_mask | person_mask;
 
+    % compute IoU (jaccard)
     frame_int_jaccard_1(i) = jaccard(full_mask, frame_1_mask);
     frame_int_jaccard_2(i) = jaccard(full_mask, frame_2_mask);
     no_integration_jaccard(i) = jaccard(full_mask, no_integration_mask);
@@ -59,6 +62,7 @@ end
 
 jaccards = {frame_int_jaccard_1, frame_int_jaccard_2, no_integration_jaccard, no_merge_jaccard, interactive_jaccard};
 
+% calculate mean IoU
 for i = 1:size(jaccards, 2)
     jaccards{i} = rmmissing(jaccards{i});
     disp(mean(jaccards{i}))
