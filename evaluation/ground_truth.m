ba7055ccfba2d2a5da7353ae8bcea588b969858e
Josh <josh.e.stein@gmail.com>
2019-10-01 18:04:02 +0200

    Add comparison to full interactive segmentation.

--- evaluation/ground_truth.m
+++ evaluation/ground_truth.m
@@ -4,11 +4,12 @@ function jaccards = ground_truth()
 % ros - 'Validation'
 gt_dir = "/home/josh/Documents/UCT/Thesis/Datasets/ground_truth_segmentation/ros_offline/KITTI_SEMANTIC/Validation_07/GT/";
 base_dir = "/home/josh/Documents/UCT/Thesis/Datasets/ground_truth_segmentation/ros_offline/KITTI_SEMANTIC/Validation_07/RGB/";
-% frame_integration_1 = "/home/josh/Documents/UCT/Thesis/Code/kitti/full_run/drive_27/1_f_1_b/no_merge/";
-% frame_integration_2 = "/home/josh/Documents/UCT/Thesis/Code/kitti/full_run/drive_27/2_b/no_merge/";
-% segmentation_dir = "/home/josh/Documents/UCT/Thesis/Code/kitti/full_run/drive_27/no_integration/";
-% no_merge_dir = "/home/josh/Documents/UCT/Thesis/Code/kitti/full_run/drive_27/no_merge/";
-% interactive_dir = "/home/josh/Documents/UCT/Thesis/Code/kitti/full_run/drive_27/no_merge/interactive/";
+frame_integration_1 = "/home/josh/Documents/UCT/Thesis/Code/kitti/full_run/drive_27/1_f_1_b/no_merge/";
+frame_integration_2 = "/home/josh/Documents/UCT/Thesis/Code/kitti/full_run/drive_27/2_b/no_merge/";
+segmentation_dir = "/home/josh/Documents/UCT/Thesis/Code/kitti/full_run/drive_27/no_integration/";
+no_merge_dir = "/home/josh/Documents/UCT/Thesis/Code/kitti/full_run/drive_27/no_merge/";
+interactive_dir = "/home/josh/Documents/UCT/Thesis/Code/kitti/full_run/drive_27/no_merge/interactive/";
+full_interactive_dir = "/home/josh/Documents/UCT/Thesis/Code/kitti/full_run/drive_27/full_interactive/";
 
 % 2_sampling
 % frame_integration_1 = "/home/josh/Documents/UCT/Thesis/Code/kitti/full_run/drive_27/2_sampling/1_f_1_b/";
@@ -25,11 +26,11 @@ base_dir = "/home/josh/Documents/UCT/Thesis/Datasets/ground_truth_segmentation/r
 % interactive_dir = "/home/josh/Documents/UCT/Thesis/Code/kitti/full_run/drive_27/3_sampling/interactive/";
 
 % 4_sampling
-frame_integration_1 = "/home/josh/Documents/UCT/Thesis/Code/kitti/full_run/drive_27/4_sampling/1_f_1_b/";
-frame_integration_2 = "/home/josh/Documents/UCT/Thesis/Code/kitti/full_run/drive_27/4_sampling/2_b/";
-segmentation_dir = "/home/josh/Documents/UCT/Thesis/Code/kitti/full_run/drive_27/4_sampling/no_integration/";
-no_merge_dir = "/home/josh/Documents/UCT/Thesis/Code/kitti/full_run/drive_27/4_sampling/no_merge/";
-interactive_dir = "/home/josh/Documents/UCT/Thesis/Code/kitti/full_run/drive_27/4_sampling/interactive/";
+% frame_integration_1 = "/home/josh/Documents/UCT/Thesis/Code/kitti/full_run/drive_27/4_sampling/1_f_1_b/";
+% frame_integration_2 = "/home/josh/Documents/UCT/Thesis/Code/kitti/full_run/drive_27/4_sampling/2_b/";
+% segmentation_dir = "/home/josh/Documents/UCT/Thesis/Code/kitti/full_run/drive_27/4_sampling/no_integration/";
+% no_merge_dir = "/home/josh/Documents/UCT/Thesis/Code/kitti/full_run/drive_27/4_sampling/no_merge/";
+% interactive_dir = "/home/josh/Documents/UCT/Thesis/Code/kitti/full_run/drive_27/4_sampling/interactive/";
 
 % 5_sampling
 % frame_integration_1 = "/home/josh/Documents/UCT/Thesis/Code/kitti/full_run/drive_27/5_sampling/1_f_1_b/";
@@ -60,6 +61,7 @@ frame_int_jaccard_2 = zeros(size(num_files, 1)-2, 1);
 no_integration_jaccard = zeros(size(num_files, 1)-2, 1);
 no_merge_jaccard = zeros(size(num_files, 1)-2, 1);
 interactive_jaccard = zeros(size(num_files, 1)-2, 1);
+full_interactive_jaccard = zeros(size(num_files, 1)-2, 1);
 
 for i = 1:num_files
     frame = files(i).name;
@@ -79,6 +81,7 @@ for i = 1:num_files
         no_integration_mask = load(sprintf('%s%d_mask.mat', segmentation_dir, frame));
         no_merge_mask = load(sprintf('%s%d_mask.mat', no_merge_dir, frame));
         interactive_mask = load(sprintf('%s%d_interactive_mask.mat', interactive_dir, frame));
+        full_interactive_mask = load(sprintf('%s%d_interactive_mask.mat', full_interactive_dir, frame));
     catch
         continue;
     end
@@ -89,6 +92,7 @@ for i = 1:num_files
     no_integration_mask = logical(no_integration_mask.mask);
     no_merge_mask = logical(no_merge_mask.mask);
     interactive_mask = logical(interactive_mask.mask);
+    full_interactive_mask = logical(full_interactive_mask.mask);
 
     r = gt_img(:,:,1);
     g = gt_img(:,:,2);
@@ -107,9 +111,10 @@ for i = 1:num_files
     no_integration_jaccard(i) = jaccard(full_mask, no_integration_mask);
     no_merge_jaccard(i) = jaccard(full_mask, no_merge_mask);
     interactive_jaccard(i) = jaccard(full_mask, interactive_mask);
+    full_interactive_jaccard(i) = jaccard(full_mask, full_interactive_mask);
 end
 
-jaccards = {frame_int_jaccard_1, frame_int_jaccard_2, no_integration_jaccard, no_merge_jaccard, interactive_jaccard};
+jaccards = {frame_int_jaccard_1, frame_int_jaccard_2, no_integration_jaccard, no_merge_jaccard, interactive_jaccard, full_interactive_jaccard};
 
 % calculate mean IoU
 for i = 1:size(jaccards, 2)
