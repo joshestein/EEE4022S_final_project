8cb560de69de99751e6bd67cb13fde11bffc993d
Josh <josh.e.stein@gmail.com>
2019-10-03 11:42:50 +0200

    Added bb IoU.

--- evaluation/ground_truth.m
+++ evaluation/ground_truth.m
@@ -10,6 +10,7 @@ segmentation_dir = "/home/josh/Documents/UCT/Thesis/Code/kitti/full_run/drive_27
 no_merge_dir = "/home/josh/Documents/UCT/Thesis/Code/kitti/full_run/drive_27/no_merge/";
 interactive_dir = "/home/josh/Documents/UCT/Thesis/Code/kitti/full_run/drive_27/no_merge/interactive/";
 full_interactive_dir = "/home/josh/Documents/UCT/Thesis/Code/kitti/full_run/drive_27/full_interactive/";
+bb_dir = "/home/josh/Documents/UCT/Thesis/Code/kitti/full_run/drive_27/bb/";
 
 % 2_sampling
 % frame_integration_1 = "/home/josh/Documents/UCT/Thesis/Code/kitti/full_run/drive_27/2_sampling/1_f_1_b/";
@@ -62,6 +63,7 @@ no_integration_jaccard = zeros(size(num_files, 1)-2, 1);
 no_merge_jaccard = zeros(size(num_files, 1)-2, 1);
 interactive_jaccard = zeros(size(num_files, 1)-2, 1);
 full_interactive_jaccard = zeros(size(num_files, 1)-2, 1);
+bb_jaccard = zeros(size(num_files, 1)-2, 1);
 
 for i = 1:num_files
     frame = files(i).name;
@@ -82,6 +84,7 @@ for i = 1:num_files
         no_merge_mask = load(sprintf('%s%d_mask.mat', no_merge_dir, frame));
         interactive_mask = load(sprintf('%s%d_interactive_mask.mat', interactive_dir, frame));
         full_interactive_mask = load(sprintf('%s%d_interactive_mask.mat', full_interactive_dir, frame));
+        bb_mask = load(sprintf('%s%d_mask.mat', bb_dir, frame));
     catch
         continue;
     end
@@ -93,6 +96,7 @@ for i = 1:num_files
     no_merge_mask = logical(no_merge_mask.mask);
     interactive_mask = logical(interactive_mask.mask);
     full_interactive_mask = logical(full_interactive_mask.mask);
+    bb_mask = logical(bb_mask.mask);
 
     r = gt_img(:,:,1);
     g = gt_img(:,:,2);
@@ -112,9 +116,10 @@ for i = 1:num_files
     no_merge_jaccard(i) = jaccard(full_mask, no_merge_mask);
     interactive_jaccard(i) = jaccard(full_mask, interactive_mask);
     full_interactive_jaccard(i) = jaccard(full_mask, full_interactive_mask);
+    bb_jaccard(i) = jaccard(full_mask, bb_mask);
 end
 
-jaccards = {frame_int_jaccard_1, frame_int_jaccard_2, no_integration_jaccard, no_merge_jaccard, interactive_jaccard, full_interactive_jaccard};
+jaccards = {frame_int_jaccard_1, frame_int_jaccard_2, no_integration_jaccard, no_merge_jaccard, interactive_jaccard, full_interactive_jaccard, bb_jaccard};
 
 % calculate mean IoU
 for i = 1:size(jaccards, 2)
