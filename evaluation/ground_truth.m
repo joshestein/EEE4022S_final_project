382cd3c92803dbecd1c86a869775e169555d4980
Josh <josh.e.stein@gmail.com>
2019-10-13 16:52:13 +0200

    Updated to new paths.

--- evaluation/ground_truth.m
+++ evaluation/ground_truth.m
@@ -2,8 +2,8 @@ function jaccards = ground_truth()
 
 % 2011_09_30_drive_27
 % ros - 'Validation'
-% gt_dir = "/home/josh/Documents/UCT/Thesis/Datasets/ground_truth_segmentation/ros_offline/KITTI_SEMANTIC/Validation_07/GT/";
-% base_dir = "/home/josh/Documents/UCT/Thesis/Datasets/ground_truth_segmentation/ros_offline/KITTI_SEMANTIC/Validation_07/RGB/";
+gt_dir = "/home/josh/Documents/UCT/Thesis/Datasets/ground_truth_segmentation/ros_offline/KITTI_SEMANTIC/Validation_07/GT/";
+base_dir = "/home/josh/Documents/UCT/Thesis/Datasets/ground_truth_segmentation/ros_offline/KITTI_SEMANTIC/Validation_07/RGB/";
 % frame_integration_1 = "/home/josh/Documents/UCT/Thesis/Code/kitti/full_run/drive_27/1_f_1_b/no_merge/";
 % frame_integration_2 = "/home/josh/Documents/UCT/Thesis/Code/kitti/full_run/drive_27/2_b/no_merge/";
 % segmentation_dir = "/home/josh/Documents/UCT/Thesis/Code/kitti/full_run/drive_27/no_integration/";
@@ -12,46 +12,46 @@ function jaccards = ground_truth()
 % full_interactive_dir = "/home/josh/Documents/UCT/Thesis/Code/kitti/full_run/drive_27/full_interactive/";
 
 % 2_sampling
-% frame_integration_1 = "/home/josh/Documents/UCT/Thesis/Code/kitti/full_run/drive_27/2_sampling/1_f_1_b/";
-% frame_integration_2 = "/home/josh/Documents/UCT/Thesis/Code/kitti/full_run/drive_27/2_sampling/2_b/";
-% segmentation_dir = "/home/josh/Documents/UCT/Thesis/Code/kitti/full_run/drive_27/2_sampling/no_integration/";
-% no_merge_dir = "/home/josh/Documents/UCT/Thesis/Code/kitti/full_run/drive_27/2_sampling/no_merge/";
-% interactive_dir = "/home/josh/Documents/UCT/Thesis/Code/kitti/full_run/drive_27/2_sampling/interactive/";
+% frame_integration_1 = "/home/josh/Documents/UCT/Thesis/Code/kitti/full_run/drive_27_2/2_sampling/1_f_1_b/";
+% frame_integration_2 = "/home/josh/Documents/UCT/Thesis/Code/kitti/full_run/drive_27_2/2_sampling/2_b/";
+% segmentation_dir = "/home/josh/Documents/UCT/Thesis/Code/kitti/full_run/drive_27_2/2_sampling/no_integration/";
+% no_merge_dir = "/home/josh/Documents/UCT/Thesis/Code/kitti/full_run/drive_27_2/2_sampling/no_merge/";
+% interactive_dir = "/home/josh/Documents/UCT/Thesis/Code/kitti/full_run/drive_27_2/2_sampling/interactive/";
 
 % 3_sampling
-% frame_integration_1 = "/home/josh/Documents/UCT/Thesis/Code/kitti/full_run/drive_27/3_sampling/1_f_1_b/";
-% frame_integration_2 = "/home/josh/Documents/UCT/Thesis/Code/kitti/full_run/drive_27/3_sampling/2_b/";
-% segmentation_dir = "/home/josh/Documents/UCT/Thesis/Code/kitti/full_run/drive_27/3_sampling/no_integration/";
-% no_merge_dir = "/home/josh/Documents/UCT/Thesis/Code/kitti/full_run/drive_27/3_sampling/no_merge/";
-% interactive_dir = "/home/josh/Documents/UCT/Thesis/Code/kitti/full_run/drive_27/3_sampling/interactive/";
+% frame_integration_1 = "/home/josh/Documents/UCT/Thesis/Code/kitti/full_run/drive_27_2/3_sampling/1_f_1_b/";
+% frame_integration_2 = "/home/josh/Documents/UCT/Thesis/Code/kitti/full_run/drive_27_2/3_sampling/2_b/";
+% segmentation_dir = "/home/josh/Documents/UCT/Thesis/Code/kitti/full_run/drive_27_2/3_sampling/no_integration/";
+% no_merge_dir = "/home/josh/Documents/UCT/Thesis/Code/kitti/full_run/drive_27_2/3_sampling/no_merge/";
+% interactive_dir = "/home/josh/Documents/UCT/Thesis/Code/kitti/full_run/drive_27_2/3_sampling/interactive/";
 
 % 4_sampling
-% frame_integration_1 = "/home/josh/Documents/UCT/Thesis/Code/kitti/full_run/drive_27/4_sampling/1_f_1_b/";
-% frame_integration_2 = "/home/josh/Documents/UCT/Thesis/Code/kitti/full_run/drive_27/4_sampling/2_b/";
-% segmentation_dir = "/home/josh/Documents/UCT/Thesis/Code/kitti/full_run/drive_27/4_sampling/no_integration/";
-% no_merge_dir = "/home/josh/Documents/UCT/Thesis/Code/kitti/full_run/drive_27/4_sampling/no_merge/";
-% interactive_dir = "/home/josh/Documents/UCT/Thesis/Code/kitti/full_run/drive_27/4_sampling/interactive/";
+% frame_integration_1 = "/home/josh/Documents/UCT/Thesis/Code/kitti/full_run/drive_27_2/4_sampling/1_f_1_b/";
+% frame_integration_2 = "/home/josh/Documents/UCT/Thesis/Code/kitti/full_run/drive_27_2/4_sampling/2_b/";
+% segmentation_dir = "/home/josh/Documents/UCT/Thesis/Code/kitti/full_run/drive_27_2/4_sampling/no_integration/";
+% no_merge_dir = "/home/josh/Documents/UCT/Thesis/Code/kitti/full_run/drive_27_2/4_sampling/no_merge/";
+% interactive_dir = "/home/josh/Documents/UCT/Thesis/Code/kitti/full_run/drive_27_2/4_sampling/interactive/";
 
 % 5_sampling
-% frame_integration_1 = "/home/josh/Documents/UCT/Thesis/Code/kitti/full_run/drive_27/5_sampling/1_f_1_b/";
-% frame_integration_2 = "/home/josh/Documents/UCT/Thesis/Code/kitti/full_run/drive_27/5_sampling/2_b/";
-% segmentation_dir = "/home/josh/Documents/UCT/Thesis/Code/kitti/full_run/drive_27/5_sampling/no_integration/";
-% no_merge_dir = "/home/josh/Documents/UCT/Thesis/Code/kitti/full_run/drive_27/5_sampling/no_merge/";
-% interactive_dir = "/home/josh/Documents/UCT/Thesis/Code/kitti/full_run/drive_27/5_sampling/interactive/";
+frame_integration_1 = "/home/josh/Documents/UCT/Thesis/Code/kitti/full_run/drive_27_2/5_sampling/1_f_1_b/";
+frame_integration_2 = "/home/josh/Documents/UCT/Thesis/Code/kitti/full_run/drive_27_2/5_sampling/2_b/";
+segmentation_dir = "/home/josh/Documents/UCT/Thesis/Code/kitti/full_run/drive_27_2/5_sampling/no_integration/";
+no_merge_dir = "/home/josh/Documents/UCT/Thesis/Code/kitti/full_run/drive_27_2/5_sampling/no_merge/";
+interactive_dir = "/home/josh/Documents/UCT/Thesis/Code/kitti/full_run/drive_27_2/5_sampling/interactive/";
 
 
 % % % % % % % % % % % % %% % % % % % % % % % % % % % % % %
 
 % 2011_10_03_drive_27
 % ros - 'Training'
-gt_dir = "/home/josh/Documents/UCT/Thesis/Datasets/ground_truth_segmentation/ros_offline/KITTI_SEMANTIC/Training_00/GT/";
-base_dir = "/home/josh/Documents/UCT/Thesis/Datasets/ground_truth_segmentation/ros_offline/KITTI_SEMANTIC/Training_00/RGB/";
-frame_integration_1 = "/home/josh/Documents/UCT/Thesis/Code/kitti/full_run/10_03_drive_27/1_f_1_b/no_merge/";
-frame_integration_2 = "/home/josh/Documents/UCT/Thesis/Code/kitti/full_run/10_03_drive_27/2_b/no_merge/";
-segmentation_dir = "/home/josh/Documents/UCT/Thesis/Code/kitti/full_run/10_03_drive_27/no_integration/";
-no_merge_dir = "/home/josh/Documents/UCT/Thesis/Code/kitti/full_run/10_03_drive_27/no_merge/";
-interactive_dir = "/home/josh/Documents/UCT/Thesis/Code/kitti/full_run/10_03_drive_27/no_merge/interactive/";
-full_interactive_dir = "/home/josh/Documents/UCT/Thesis/Code/kitti/full_run/10_03_drive_27/full_interactive/";
+% gt_dir = "/home/josh/Documents/UCT/Thesis/Datasets/ground_truth_segmentation/ros_offline/KITTI_SEMANTIC/Training_00/GT/";
+% base_dir = "/home/josh/Documents/UCT/Thesis/Datasets/ground_truth_segmentation/ros_offline/KITTI_SEMANTIC/Training_00/RGB/";
+% frame_integration_1 = "/home/josh/Documents/UCT/Thesis/Code/kitti/full_run/10_03_drive_27/1_f_1_b/no_merge/";
+% frame_integration_2 = "/home/josh/Documents/UCT/Thesis/Code/kitti/full_run/10_03_drive_27/2_b/no_merge/";
+% segmentation_dir = "/home/josh/Documents/UCT/Thesis/Code/kitti/full_run/10_03_drive_27/no_integration/";
+% no_merge_dir = "/home/josh/Documents/UCT/Thesis/Code/kitti/full_run/10_03_drive_27/no_merge/";
+% interactive_dir = "/home/josh/Documents/UCT/Thesis/Code/kitti/full_run/10_03_drive_27/no_merge/interactive/";
+% full_interactive_dir = "/home/josh/Documents/UCT/Thesis/Code/kitti/full_run/10_03_drive_27/full_interactive/";
 
 
 files = dir(gt_dir);
@@ -62,7 +62,7 @@ frame_int_jaccard_2 = zeros(size(num_files, 1)-2, 1);
 no_integration_jaccard = zeros(size(num_files, 1)-2, 1);
 no_merge_jaccard = zeros(size(num_files, 1)-2, 1);
 interactive_jaccard = zeros(size(num_files, 1)-2, 1);
-full_interactive_jaccard = zeros(size(num_files, 1)-2, 1);
+% full_interactive_jaccard = zeros(size(num_files, 1)-2, 1);
 
 for i = 1:num_files
     frame = files(i).name;
@@ -82,7 +82,7 @@ for i = 1:num_files
         no_integration_mask = load(sprintf('%s%d_mask.mat', segmentation_dir, frame));
         no_merge_mask = load(sprintf('%s%d_mask.mat', no_merge_dir, frame));
         interactive_mask = load(sprintf('%s%d_interactive_mask.mat', interactive_dir, frame));
-        full_interactive_mask = load(sprintf('%s%d_interactive_mask.mat', full_interactive_dir, frame));
+        % full_interactive_mask = load(sprintf('%s%d_interactive_mask.mat', full_interactive_dir, frame));
     catch
         continue;
     end
@@ -93,7 +93,7 @@ for i = 1:num_files
     no_integration_mask = logical(no_integration_mask.mask);
     no_merge_mask = logical(no_merge_mask.mask);
     interactive_mask = logical(interactive_mask.mask);
-    full_interactive_mask = logical(full_interactive_mask.mask);
+    % full_interactive_mask = logical(full_interactive_mask.mask);
 
     r = gt_img(:,:,1);
     g = gt_img(:,:,2);
@@ -112,10 +112,12 @@ for i = 1:num_files
     no_integration_jaccard(i) = jaccard(full_mask, no_integration_mask);
     no_merge_jaccard(i) = jaccard(full_mask, no_merge_mask);
     interactive_jaccard(i) = jaccard(full_mask, interactive_mask);
-    full_interactive_jaccard(i) = jaccard(full_mask, full_interactive_mask);
+    % full_interactive_jaccard(i) = jaccard(full_mask, full_interactive_mask);
 end
 
-jaccards = {frame_int_jaccard_1, frame_int_jaccard_2, no_integration_jaccard, no_merge_jaccard, interactive_jaccard, full_interactive_jaccard};
+% jaccards = {frame_int_jaccard_1, frame_int_jaccard_2, no_integration_jaccard, no_merge_jaccard, interactive_jaccard, full_interactive_jaccard};
+
+jaccards = {no_merge_jaccard, no_integration_jaccard, frame_int_jaccard_1, frame_int_jaccard_2, interactive_jaccard};
 
 % calculate mean IoU
 for i = 1:size(jaccards, 2)
